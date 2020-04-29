5e7d36df22b945f2f1b9fdf33aff7d8773f11217
jaliagag <40398975+jaliagag@users.noreply.github.com>
2020-04-29 19:45:11 -0300

    suggested results only display and one random gif coming through

--- TP02_2do intento/scripts/app.js
+++ TP02_2do intento/scripts/app.js
@@ -1,59 +1,25 @@
-/*
-SUGERENCIAS
+const apikey = "dkElzr6SJfgkAbeHk3kGa70yrOiwrdcP";
+const laTag ="homer"
+const aleatorio = `https://api.giphy.com/v1/gifs/random?api_key=${apikey}&limit=1&tag=${laTag}`;
+const resultsEl = document.getElementById("gifsTende");
 
-buscamos los elementos random
-*/
-
-
-
-const APIKEY = "dkElzr6SJfgkAbeHk3kGa70yrOiwrdcP";
-const QUERY = "cats"; // value from input
-const PATH= `https://api.giphy.com/v1/gifs/search?api_key=${APIKEY}&q=${QUERY}`;
-//const PATH = 
-
-fetch(PATH)
-.then((res) => {
-    return res.json()
-})
-.then((json) => {
-    console.log(json.data[0].images)
-    let unGif = json.data[0].images.original
-    const LOSGIFS01 = document.getElementById("gifsTende01")
-    let paraElHTML = unGif
-
-
-
-
-    LOSGIFS01.innerHTML = paraElHTML
-    /*console.log(json.data[0].images.fixed_width.url)
-    const LOSGIFS = document.getElementById("gifsTende")
-    let paraElHTML = ""
-
-    json.data.forEach((obj) => {
-        console.log(obj.images.fixed_width.url);
-        const elGif = obj.images.fixed_width.url
-        const elAlt = obj.title
-        paraElHTML += `<img src="${elGif}" alt="gif" width="280px" height="280" alt="${elAlt}">`
-    })
-
-
-    LOSGIFS.innerHTML = paraElHTML*/
-})
-.catch((err) => {
+fetch(aleatorio).then((res)=>{
+    return res.json();
+}).then((json) => {
+    let resultsHTML = json.data.image_original_url;
+    let asddf = "";
+    asddf += `<img src="${resultsHTML}" alt="gif aleatorio" z-index=0>`;
+    resultsEl.innerHTML = asddf;
+    
+}).catch((err) => {
     console.log(err.message);
 })
 
+// 
+var queBuscar = document.getElementById("")
+var buscar = document.getElementById("btnBuscar");
+que.addEventListener("submit", leSearch());
 
-
-
-/*
-? --> starts the earch
-& add another parameter
-q=
-
-
-
-
-fetch("api.giphy.com/v1/gifs/random")
-.then(res => res.json())
-.then(data => console.log(data))*/
+function leSearch(q) {
+  console.log(que.value);
+}
